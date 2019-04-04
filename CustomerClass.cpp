43fbff048cdbd89bbefde99f92fbd61c67fc544f
Khrys1129 <49284190+Khrys1129@users.noreply.github.com>
2019-04-04 11:47:55 -0600

    Update CustomerClass.cpp

--- CustomerClass.cpp
+++ CustomerClass.cpp
@@ -1,1 +1,10 @@
-//implement all customer member functions here
+//
+//  CustomerClass.hpp
+//  Final_Project_for_DataStructures
+//
+//  Created by Christopher Schutte & Logan Mann on 4/2/19.
+//  Copyright © 2019 Christopher Schutte & Logan Mann. All rights reserved.
+//
+
+#include "CustomerClass.hpp"
+
