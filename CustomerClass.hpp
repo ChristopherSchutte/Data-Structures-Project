d0db77082c2df3319d75b67a6b18437fc651aba2
Khrys1129 <49284190+Khrys1129@users.noreply.github.com>
2019-04-04 11:31:55 -0600

    Update CustomerClass.hpp

--- CustomerClass.hpp
+++ CustomerClass.hpp
@@ -2,8 +2,8 @@
 //  CustomerClass.hpp
 //  Final_Project_for_DataStructures
 //
-//  Created by Christopher Schutte on 4/2/19.
-//  Copyright © 2019 Christopher Schutte. All rights reserved.
+//  Created by Christopher Schutte & Logan Mann on 4/2/19.
+//  Copyright © 2019 Christopher Schutte & Logan Mann. All rights reserved.
 //
 
 #ifndef CustomerClass_hpp
@@ -18,6 +18,7 @@
 struct Product
 {
     std::string product_name, catagory, color, date_purchased; //product info
+    int num_purchased; //number of specific product purchased
     
     Product *previous, *next; //linked list
 };
@@ -25,9 +26,9 @@ struct Product
 struct Customer
 {
     std::string email, address, customer_name, birthday; //customer information
-    int num_products;                    //number of owned products
+    int num_products;//number of owned products
     
-    Product *purchased_head; //linked list sorted by catagory and data bought, ie, catagories will be 1,2,3,4 and if it was bought sooner then it comes first in the list
+    Product *purchased_head; //linked list sorted by catagory and date bought, ie, catagories will be 1,2,3,4 and if it was bought sooner then it comes first in the list
     Customer *next; //linked list for hash collisions
 };
 
@@ -36,13 +37,23 @@ class VIP_Tracker
     public:
         VIP_Tracker(int tablesize); //constructor
         ~VIP_Tracker(); //destructor
+    
         unsigned int HashCustomer(int email); //takes in the int version of the email string to be hashed
         unsigned int HashProduct(int catagory); //takes in the int verson of the catagory string to be hashed
+    
         Product* searchProducts(std::string name, std::string catagory); //searches hash table for specific product
         Customer* searchCustomer(std::string email, std::string name); //searches hash for specific person
-        void printAllCustomers(); //Prints all customers
-        void printSpecifcCust(std::string email, std::string name); //Prints the specifics of a certain customer
     
+        void allCustomers(); //Prints all customers
+        void specifcCust(Customer* customer); //Prints the specifics of a certain customer
+        void cusomerAnalytics(Customer* customer);
+        void mostBoughtItems(Customer* customer); //Prints the most bought items by item name
+        void mostBoughtCatagory(Customer* customer); //Prints most bought from catagory
+        void numberProductsOwned(Customer* customer); //Prints number of products owned by customer
+        void mostPopularItems(Product* product); //Prints the 5 most bought products
+        void whoBoughtItem(Product* product); //Prints the all customers who bought specific product
+    
+        void reccLikeProdcut(Customer* customer); //reccomends a product in the same catagory as the most bought
     
     private:
         int tablesize;
