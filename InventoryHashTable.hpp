bf551f71e5d0f6bba6d7df337422b46f64b873c4
Khrys1129 <49284190+Khrys1129@users.noreply.github.com>
2019-04-17 15:12:12 -0600

    Update InventoryHashTable.hpp

--- InventoryHashTable.hpp
+++ InventoryHashTable.hpp
@@ -1,40 +1,33 @@
-#ifndef INVENTORY_HASH_HPP
-#define INVENTORY_HASH_HPP
-
 #include <iostream>
 #include <string>
 
 
-struct Product
+struct InventoryProduct
 {
-    std::string product_name, catagory, color, date_purchased; //product info
+    std::string product_name, catagory, color, size; //product info
     int num_purchased; //number of specific product purchased
     
-    Product *previous, *next; //linked list
+    InventoryProduct *previous, *next; //linked list
 };
 
-class Inventory
+class Inventory //Possibly make one inventory
 {
     private:
-    int I_tablesize;
-    Product* *all_products;
-    Product* *purchased_products;
+    int numCatagorys;
+    InventoryProduct* *fullInventory;
     
     public:
-    Inventory(int I_tablesize);
+    Inventory(int Catagorys);
     ~Inventory();
-    unsigned int HashProduct(int catagory);
+    unsigned int HashProduct(std::string catagory);
     
-    Product *search_purchased(std::string product_name, std::string catagory);
-    Product *search_allProducts(std::string product_name, std::string catagory);
+    InventoryProduct *searchInventory(std::string product_name, std::string catagory, std::string color, std::string size);
     
     void print_purchased();
     void print_allProducts();
     
-    void print_numPurchased(std::string product_name, std::string catagory);
-    void print_topCatagorys();
-    void print_topColors();
+    void print_numPurchased(std::string product_name, std::string catagory, std::string color, std::string size);
     
-    bool addProduct(std::string product_name, std::string catagory, std::string color, Product* next);
-    bool addPurchasedProduct(std::string product_name, std::string catagory, std::string color, std::string date_purchased, Product* next);
+    bool addProduct(std::string product_name, std::string catagory, std::string color, std::string size, InventoryProduct* next);
+    bool addPurchasedProduct(std::string product_name, std::string catagory, std::string color, std::string size, InventoryProduct* next);
 };
