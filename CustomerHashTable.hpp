cbb7c8ec0c23956c346fb0992709a5b3415f48a9
logan-mann <49215056+logan-mann@users.noreply.github.com>
2019-04-17 22:43:06 -0600

    Update CustomerHashTable.hpp

--- CustomerHashTable.hpp
+++ CustomerHashTable.hpp
@@ -7,7 +7,7 @@ struct Product
 {
     std::string product_name, catagory, color, date_purchased; //product info
     int num_purchased; //number of specific product purchased
-    
+
     Product *previous, *next; //linked list
 };
 
@@ -15,7 +15,7 @@ struct Customer
 {
     std::string email, address, customer_name, birthday; //customer information
     int num_products;//number of owned products
-    
+
     Product *purchased_head; //linked list sorted by catagory and date bought, ie, catagories will be 1,2,3,4 and if it was bought sooner then it comes first in the list
     Customer *next; //linked list for hash collisions
 };
@@ -24,15 +24,15 @@ class CustomerHashTable
 {
   public:
     //constructor, tablesize = max size of hash table
-    CustomerHashTable (int tablesize);
+    CustomerHashTable (int newTablesize);
     //destructor
     ~CustomerHashTable();
     //adds a customer w/ info passed to the table
     void addCustomer(std::string email, std::string address, std::string customer_name, std::string birthday);
-    //removes a customer from the hash table
+    //removes customer from hash table
     void removeCustomer(std::string email);
     //adds a purchase for the customer with the email passed
-    void addPurchase(std::string custEmail, std::string productName, std::string catagory, std::string color, std::string date_purchased);
+    void addPurchase(std::string custEmail, std::string productName, std::string catagory, std::string color, std::string date_purchased); //IMPORTANT ADD COLOR PARAMETER
     //searches the hash table for a specific customer, returns pointer to that customer
     Customer* searchCustomer(std::string email);
   private:
