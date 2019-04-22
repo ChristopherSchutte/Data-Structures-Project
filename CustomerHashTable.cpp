cf8bff184e5b1e0e0fb00c2f088975dadd5538d2
logan-mann <49215056+logan-mann@users.noreply.github.com>
2019-04-22 03:44:50 -0700

    Add files via upload

--- CustomerHashTable.cpp
+++ CustomerHashTable.cpp
@@ -1,6 +1,7 @@
 #include "CustomerHashTable.hpp"
 #include <cmath>
 #include <iostream>
+#include <fstream>
 
 CustomerHashTable::CustomerHashTable(int newTablesize)
 {
@@ -50,7 +51,7 @@ unsigned int CustomerHashTable::hashCust(std::string email)
     return (hashValue % tablesize);
 }
 
-void CustomerHashTable::addCustomer(std::string email, std::string address, std::string customer_name, std::string birthday)
+void CustomerHashTable::addCustomer(std::string email, std::string customer_name)
 {
   if (searchCustomer(email) != NULL)
   {
@@ -61,11 +62,9 @@ void CustomerHashTable::addCustomer(std::string email, std::string address, std:
   Customer* newCust = new Customer;
   newCust->customer_name = customer_name;
   newCust->email = email;
-  newCust->address = address;
-  newCust->birthday = birthday;
 
   newCust->num_products = 0;
-  newCust->purchased_head = NULL;
+  //newCust->purchased_head = NULL;
   newCust->next = NULL;
 
 
@@ -105,6 +104,8 @@ void CustomerHashTable::removeCustomer(std::string delEmail)
 
 Product* findProductInPurchased(Customer* customer, std::string productName)
 {
+  return (customer->purchasedProducts.findProduct(productName));
+  /*
   Product* pres = customer->purchased_head;
   if (pres != NULL)
   {
@@ -115,9 +116,10 @@ Product* findProductInPurchased(Customer* customer, std::string productName)
   }
 
   return pres;
+  */
 }
 
-void CustomerHashTable::addPurchase(std::string custEmail, std::string productName, std::string catagory, std::string color, std::string date_purchased)
+void CustomerHashTable::addPurchase(std::string custEmail, std::string productName, std::string catagory, std::string color, std::string size)
 {
   Customer* customer = searchCustomer(custEmail);
   if (customer == NULL)
@@ -128,20 +130,24 @@ void CustomerHashTable::addPurchase(std::string custEmail, std::string productNa
   Product* product = findProductInPurchased(customer, productName);
   if (product == NULL)
   {
+    customer->num_products++;
     Product* newProduct = new Product;
     newProduct->product_name = productName;
     newProduct->catagory = catagory;
     newProduct->color = color;
-    newProduct->date_purchased = date_purchased;
-    newProduct->next = customer->purchased_head;
+    newProduct->size = size;
+    //newProduct->next = customer->purchased_head;
     newProduct->num_purchased = 1;
-    customer->purchased_head = newProduct;
+    (customer->purchasedProducts).addToHeap(newProduct);
+    //customer->purchased_head = newProduct;
     return;
+
   }
   else
   {
-    product->date_purchased = date_purchased;
     product->num_purchased = product->num_purchased + 1;
+    (customer->purchasedProducts).repairFromChangedProduct(product);
+    customer->num_products++;
     return;
   }
 }
@@ -160,4 +166,26 @@ Customer* CustomerHashTable::searchCustomer(std::string findEmail)
   }
   return pres;
 
+}
+
+void CustomerHashTable::writeToFile(std::string writeFileName)
+{
+  std::ofstream writeFile;
+  writeFile.open(writeFileName);
+  Customer* currCust;
+  Product* currPurchase;
+  for (int i = 0; i < tablesize; i++)
+  {
+    currCust = all_Customers[i];
+    while (currCust != NULL)
+    {
+      currCust->purchasedProducts.writeToFile(writeFileName, currCust->email, currCust->customer_name);
+      currCust = currCust->next;
+    }
+  }
+}
+
+Customer* CustomerHashTable::getCustomerAtIndex(int index)
+{
+  return all_Customers[index];
 }
--- CustomerHashTable.hpp
+++ CustomerHashTable.hpp
@@ -2,21 +2,24 @@
 #define CUSTOMER_HASH_HPP
 
 #include <iostream>
-
+#include "PurchasedHeap.hpp"
+/*
 struct Product
 {
     std::string product_name, catagory, color, date_purchased; //product info
     int num_purchased; //number of specific product purchased
 
-    Product *previous, *next; //linked list
+    //Product *previous, *next; //linked list
 };
+*/
 
 struct Customer
 {
-    std::string email, address, customer_name, birthday; //customer information
-    int num_products;//number of owned products
-
-    Product *purchased_head; //linked list sorted by catagory and date bought, ie, catagories will be 1,2,3,4 and if it was bought sooner then it comes first in the list
+    std::string email, customer_name; //customer information
+    int num_products = 0;//number of owned products
+    //max heap of purchased products
+    PurchasedHeap purchasedProducts = PurchasedHeap(500);
+    //Product *purchased_head; //linked list sorted by catagory and date bought, ie, catagories will be 1,2,3,4 and if it was bought sooner then it comes first in the list
     Customer *next; //linked list for hash collisions
 };
 
@@ -28,13 +31,17 @@ class CustomerHashTable
     //destructor
     ~CustomerHashTable();
     //adds a customer w/ info passed to the table
-    void addCustomer(std::string email, std::string address, std::string customer_name, std::string birthday);
+    void addCustomer(std::string email, std::string customer_name);
     //removes customer from hash table
     void removeCustomer(std::string email);
     //adds a purchase for the customer with the email passed
-    void addPurchase(std::string custEmail, std::string productName, std::string catagory, std::string color, std::string date_purchased); //IMPORTANT ADD COLOR PARAMETER
+    void addPurchase(std::string custEmail, std::string productName, std::string catagory, std::string color, std::string size); //IMPORTANT ADD COLOR PARAMETER
     //searches the hash table for a specific customer, returns pointer to that customer
     Customer* searchCustomer(std::string email);
+    //writes data to file
+    void writeToFile(std::string writeFileName);
+
+    Customer* getCustomerAtIndex(int index);
   private:
     //hashes customer email passed
     unsigned int hashCust(std::string email);
